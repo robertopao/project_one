b40d7c21fc226d100c57f5d334b601590150a73d
robertopao <robertopao@live.com>
2018-11-27 17:26:52 +0100

    converting dcm to mif done

--- src/examplepackage/features.py
+++ src/examplepackage/features.py
@@ -8,6 +8,11 @@ along with any more generic functions.
 Consider moving generic functions into the shared statoilds package.
 """
 import pandas as pd
+import multiprocessing as mp
+import random
+import string
+import os
+
 
 
 def my_feature_xxx(df: pd.DataFrame):
@@ -26,3 +31,54 @@ def my_feature_xxx(df: pd.DataFrame):
     # CODE HERE
 
     return df
+
+
+
+def run_pipeline():
+    """
+    Run the main processing pipeline.
+
+    Returns:
+        A dataframe containing the output of the pipeline
+    """
+
+    # io = IO(path)
+    # df = io.load_cleaned_file(download_always=False)
+    # df = add_choke_events(df)
+
+    # Add calls to features.Xxx here
+
+    directory = '/home/visionlab/Desktop/dMRI_data_harmonization'
+    site=os.listdir(directory)
+    site_dicom={}
+    site_dicom_sub={}
+    site_sub_files={}
+    i,k,j=0,0,0
+    for filename in site:
+        site_dicom[i]=directory+'/'+filename+'/DICOM-raw'
+        temporary_path=os.listdir(site_dicom[i])
+
+        for another_file in temporary_path:
+            site_dicom_sub[j]=site_dicom[i]+'/'+another_file+'/scans'
+            temporary_path_1 = os.listdir(site_dicom_sub[j])
+            for another_file_1 in temporary_path_1:
+                site_sub_files[k]=site_dicom_sub[j]+'/'+another_file_1+'/'
+                k=k+1
+            j = j + 1
+        i=i+1
+    splitted={}
+    output_mif={}
+    for i in range (len(site_sub_files)):
+        splitted[i]=site_sub_files[i].split('/')
+        output_mif[i]=directory+'/'+splitted[i][5]+'/MIF-raw/'+splitted[i][5]+'_'+splitted[i][7]+'_'+splitted[i][9]+'.mif'
+
+
+    # save (or return) dataframe here?
+    return site_sub_files,output_mif
+
+def convert_dcm2mif(site_sub_files,output_mif):
+    for i in range(len(site_sub_files)):
+        bashCommand = ("/home/visionlab/mrtrix3/bin/mrconvert " + site_sub_files[i]+" "+ output_mif[i])
+        os.system(bashCommand)
+    return
+
--- src/examplepackage/pipeline.py
+++ src/examplepackage/pipeline.py
@@ -6,58 +6,30 @@ that it can be reused and easily reproduced.
 """
 import pandas as pd
 import os
+from multiprocessing import Pool
+import random
+import string
+import subprocess
 
 from examplepackage import features
 from examplepackage.i_o import IO
-from examplepackage.examplemodule import  hello_world
-
-
-def run_pipeline(local_data_path: object) -> object:
-    """
-    Run the main processing pipeline.
-
-    Returns:
-        A dataframe containing the output of the pipeline
-    """
-
-    # io = IO(path)
-    # df = io.load_cleaned_file(download_always=False)
-    # df = add_choke_events(df)
-
-    # Add calls to features.Xxx here
-
-    directory = '/home/visionlab/Desktop/dMRI data harmonization'
-    site=os.listdir(directory)
-    site_dicom={}
-    site_dicom_sub={}
-    site_sub_files={}
-    i,k,j=0,0,0
-    for filename in site:
-        site_dicom[i]=directory+'/'+filename+'/DICOM-raw'
-        temporary_path=os.listdir(site_dicom[i])
-
-        for another_file in temporary_path:
-            site_dicom_sub[j]=site_dicom[i]+'/'+another_file+'/scans'
-            temporary_path_1 = os.listdir(site_dicom_sub[j])
-            for another_file_1 in temporary_path_1:
-                site_sub_files[k]=site_dicom_sub[j]+'/'+another_file_1
-                k=k+1
-            j = j + 1
-        i=i+1
-    splitted={}
-    output_mif={}
-    for i in range (len(site_sub_files)):
-        splitted[i]=site_sub_files[i].split('/')
-        output_mif[i]=directory+'/'+splitted[i][5]+'/MIF-raw/'+splitted[i][5]+'_'+splitted[i][7]+'_'+splitted[i][9]+'.mif'
-
-    string_to_print=hello_world()
-    print(string_to_print)
-    print("hi")
+from examplepackage.examplemodule import hello_world
+from examplepackage.features import my_feature_xxx,run_pipeline,convert_dcm2mif
 
 
 
-    # save (or return) dataframe here?
-    return string_to_print
+
+def main():
+    site_sub_files,output_mif = run_pipeline()
+
+    print(output_mif)
+    convert_dcm2mif(site_sub_files,output_mif)
 
 
-okkkk=run_pipeline('/home/visionlab/Desktop/dati')
+
+if __name__ == '__main__':
+    main()
+
+
+
+
